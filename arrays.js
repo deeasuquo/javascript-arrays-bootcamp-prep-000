c3cd249be8176845329393624e7949367a29f2c8
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-12 19:02:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  return array.slice(0,array.length)
+  return array.slice(0,array.length-1)
 }
