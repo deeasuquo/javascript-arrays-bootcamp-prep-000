b4d145ea9c58782b12207930d13befdd6db5f56d
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-12 18:23:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,11 +7,13 @@ function addElementToBeginningOfArray(array,element){
   // return  newArray
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-  return array.unshift(element)
+ array.unshift(element)
+ return array
 }
 function addElementToEndOfArray(array,element){
   return [...array,element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
